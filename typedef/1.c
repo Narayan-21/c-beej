295902636c66fdfe14b0395c9f9b59919b073051
Narayan-21 <naaidjan.19@gmail.com>
2025-05-08 23:55:33 +0530

    typedef init

---
+++ typedef/1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef int antelope;
+
+typedef int antelope, bagel, mushroom;
+
+int main() {
+    antelope x = 10;
+    bagel y = 20;
+    mushroom z = 30;
+
+    printf("x = %d, y = %d, z = %d\n", x, y, z);
+    return 0;
+}
