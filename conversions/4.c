c8757cc34a9ab45cb3e9aeff844f6589f882c5a7
Narayan-21 <naaidjan.19@gmail.com>
2025-05-16 19:04:21 +0530

    char conversions

---
+++ conversions/4.c
@@ -0,0 +1,12 @@
+// char conversions
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char c = '6';
+    int x = c;
+    int y = c - '0';
+    printf("%d %d \n", x, y);
+    return 0;
+}
