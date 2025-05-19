cbc71a2ea04019602895ca8debf433833bfac773
Narayan-21 <naaidjan.19@gmail.com>
2025-05-19 10:58:38 +0530

    qualifiers - const

--- qualifiers and specifiers/1.c
+++ qualifiers and specifiers/1.c
@@ -10,4 +10,10 @@ void main(void) {
     // *p = 30; -> Cannot do this -> this will throw an error
 
     // const int *p <=> int const *p (both are same)
+
+    // Changing what pointer points to but not the pointer itself.
+    int y = 30;
+    int *const p2 = &y;
+    *p2 = 20; // Now, this will be set to 20;
+    // p2++; // this will give the compile error.
 };
