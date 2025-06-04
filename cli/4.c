29842d83fa48511e2a73cf1231d6dc50a0edaa89
Narayan-21 <naaidjan.19@gmail.com>
2025-06-04 12:18:06 +0530

    environment var

---
+++ cli/4.c
@@ -0,0 +1,13 @@
+// In unix-like system - extern char **environ; => its an array of strings terminated by NULL pointer.
+#include <stdio.h>
+
+extern char **environ; // MUST be extern AND named "environ"
+
+int main(void) {
+    for (char **p = environ; *p != NULL; p++) {
+        printf("%s\n", *p);
+    };
+    for (int i = 0; environ[i] != NULL; i++) {
+        printf("%s\n", environ[i]);
+    }
+}
